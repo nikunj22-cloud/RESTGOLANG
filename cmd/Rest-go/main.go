3d47354cba8240bcdf8e60cf9078c1150884452a
nikunjvarshney <nikunjvarshneya@gmail.com>
2026-01-13 00:56:18 +0530

    go-apis

---
+++ cmd/Rest-go/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/nikunj/rest-api/internal/config"
+	"github.com/nikunj/rest-api/internal/config/http/handlers/student"
+)
+
+func main() {
+
+	//config set up
+	cfg := config.MustLoad()
+	//database setup
+	//setup router
+	router := http.NewServeMux()
+	router.HandleFunc("POST /api/students", student.New())
+	//w is response  r is request
+
+	//set up server
+
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+	fmt.Printf("server started %s", cfg.HTTPServer.Addr)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatal("error in server")
+		}
+	}()
+	<-done
+	slog.Info("shutting down")
+
+	ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancle()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Info("failed to shutting down the server", slog.String("error", err.Error()))
+	}
+	slog.Info("shutting down successfully")
+}
